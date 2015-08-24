6bbd679455dd91152f4131212d57c45569fe39c2
Kristoph Matthews <krist.m@gmail.com>
2015-08-24 16:40:11 -0400

    redirect newly signed on users to users#show, created show view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Sets redirect after successful Devise sign in.
+  def after_sign_in_path_for(resource)
+    user_path(@user)
+  end
 end
